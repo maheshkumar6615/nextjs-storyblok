6a4bd76a587c89b3b5a5e477f1ddb8a9d02316c8
Mahesh kumar Yedlapalli <mahesh@Maheshs-MBP.lan>
2025-02-24 16:11:47 -0500

    code updates

--- src/app/components/ProductDetails.tsx
+++ src/app/components/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface ProductDetailsProps {
   image: string;
@@ -10,7 +11,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ image, title, descripti
   return (
     <div className="flex flex-col md:flex-row items-center p-4 gap-4">
       <div className="w-full md:w-1/2">
-        <img src={image} alt={title} className="w-full h-auto rounded-lg shadow-md" />
+        <Image src={image} alt={title} height={600} width={600} className="w-full h-auto rounded-lg shadow-md" />
       </div>
       <div className="w-full md:w-1/2 flex flex-col gap-2">
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import ProductDetails from "../../components/ProductDetails"; // Import the ProductDetails component
+import ProductDetails from "../../components/ProductDetails";
 import { getStoryblokApi } from "@storyblok/react/rsc";
-import Image from 'next/image'; // Import the Next.js Image component
 
 const fetchProductPage = async (slug: string) => {
   const client = getStoryblokApi();
@@ -29,17 +28,6 @@ const ProductPage = async ({ params }: { params: { slug: string } }) => {  // Co
         title={story.content.Title}
         description={story.content.Description}
       />
-
-       {/* Example usage of Next.js Image component */}
-      {story.content.Image && (
-        <Image
-          src={story.content.Image.filename}
-          alt={story.content.Title}
-          width={500} // Set appropriate width
-          height={300} // Set appropriate height
-          priority // Optional: prioritize loading this image
-        />
-      )}
     </div>
   );
 };
